32689157c71bfb9eed57a6326509bfcb9573dca5
Sagar Kumar <57829219+sgrkmr@users.noreply.github.com>
2019-11-17 23:47:26 +0530

    Update __main__.py

--- votmapi/__main__.py
+++ votmapi/__main__.py
@@ -21,7 +21,7 @@ Change ENV_KEY & SECRET_KEY below for use.
 """
 
 __author__ = 'Sagar Kumar'
-__version__ = '1.0.4 (2019-2020)'
+__version__ = '1.0.4'
 
 ENV_KEY = 'KEY_NAME'
 SECRET_KEY = '<KEY_HERE>'
