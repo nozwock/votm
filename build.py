148ce5cba5b55f37b94aaf6e07cd879df439ed06
Sagar Kumar <57829219+sgrkmr@users.noreply.github.com>
2020-10-09 15:47:16 +0530

    dev: Update Build files

--- build.py
+++ build.py
@@ -5,15 +5,10 @@ import subprocess
 from sys import argv
 from pathlib import Path
 from spec._data import BASE_DIR
-from rich.console import Console
-from rich.traceback import install
 from votm import __version__
 
-install()
 
 start = time.time()
-console = Console()
-
 argv = " ".join(argv[1:])
 
 SPEC_FILE = [
@@ -37,11 +32,12 @@ for i, j in enumerate(SPEC_FILE):
 
 os.chdir(BASE_DIR)
 command = [["pyinstaller", i, "--noconfirm", argv] for i in TEMP_FILE]
+if argv.strip() == "":
+    command = [i[:-1] for i in command]
 for i, j in enumerate(command):
     try:
-        console.print(
-            f"[orange1]•[/orange1] [green3]"
-            f"Generating Build [deep_sky_blue1][{i+1}/{len(command)}][/deep_sky_blue1][/green3]...",
+        print(
+            f"• Generating Build [{i+1}/{len(command)}]...",
             end="",
         )
         proc = subprocess.run(
@@ -51,10 +47,10 @@ for i, j in enumerate(command):
             stderr=subprocess.PIPE,
             universal_newlines=True,
         )
-        console.print("ok", end=" ")
-        console.print(f"[u]ReturnCode:[/u] {proc.returncode}")
+        print("ok", end=" ")
+        print(f"ReturnCode: {proc.returncode}")
     except subprocess.CalledProcessError as e:
-        console.print(f" [bold red1]Error:[/bold red1] {e.returncode}")
+        print(f" Error: {e.returncode}")
         raise
 
 dist_dir = BASE_DIR.joinpath("dist")
@@ -63,7 +59,7 @@ target = dist_dir.joinpath(f"votm_dist_{__version__}_manage")
 
 counter = 0
 
-console.print("[orange1]•[/orange1] [green3]Collecting...")
+print("• Collecting...")
 # collecting
 for obj in source.iterdir():
     if obj.is_file():
@@ -84,13 +80,11 @@ except FileExistsError:
     )
     target.rename(target.parent.joinpath(f"votm_dist_{__version__}"))
 
-console.print("[orange1]•[/orange1] [green3]Cleaning up...")
+print("• Cleaning up...")
 # cleaning up
 shutil.rmtree(str(source), ignore_errors=True)
 for i in TEMP_FILE:
     Path(i).unlink()
 
 end = time.time()
-console.print(
-    f"[orange1]Build compeletion in[/orange1] [u]{end-start}[/u] [green3]seconds[/green3]"
-)
+print(f"Build compeletion in {end-start} seconds")
